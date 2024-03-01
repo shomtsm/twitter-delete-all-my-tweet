d3f0785d9a9692f4797d9cce8cb34aae90b9560f
Sho Matsushima <sho.mtsm@gmail.com>
2024-03-01 19:29:00 +0900

    add sample

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+import requests
+from requests_oauthlib import OAuth1
+
+# APIキーとトークンの設定
+API_KEY = 'YOUR_API_KEY'
+API_SECRET_KEY = 'YOUR_API_SECRET_KEY'
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+ACCESS_TOKEN_SECRET = 'YOUR_ACCESS_TOKEN_SECRET'
+
+# OAuth認証
+auth = OAuth1(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# アーカイブデータからツイートIDを抽出する関数
+def extract_tweet_ids_from_archive(archive_file_path):
+    tweet_ids = []
+    with open(archive_file_path, 'r', encoding='utf-8') as file:
+        data = file.read()
+        data = data.replace('window.YTD.tweets.part0 = ', '')
+        tweets_data = json.loads(data)
+
+        for tweet in tweets_data:
+            tweet_id = tweet['tweet']['id_str']
+            tweet_ids.append(tweet_id)
+    return tweet_ids
+
+# ツイートを削除する関数
+def delete_tweets(tweet_ids):
+    url_delete_tweet = "https://api.twitter.com/1.1/statuses/destroy/"
+    for tweet_id in tweet_ids:
+        del_response = requests.post(url_delete_tweet + f"{tweet_id}.json", auth=auth)
+        if del_response.status_code == 200:
+            print(f"Deleted tweet ID: {tweet_id}")
+        else:
+            print(f"Failed to delete tweet ID: {tweet_id}, Status Code: {del_response.status_code}")
+
+# 実行
+archive_file_path = 'tweets-sample.js'  # アーカイブファイルのパスを指定
+tweet_ids = extract_tweet_ids_from_archive(archive_file_path)
+delete_tweets(tweet_ids)
